d53bd1d2028e880c054e432e6dd5a9e95e2cff79
Toki <kingfisherzh@gmail.com>
2021-10-17 23:46:16 -0400

    1

--- app/api/pay.py
+++ app/api/pay.py
@@ -17,6 +17,7 @@ from app.models.session import (
 @post('/pay')
 @logged_in
 def do_payment(db, session):
+    session_id = session.get_id()
     sender = get_user(db, session.get_username())
     recipient = db.execute(
         "SELECT * FROM users WHERE users.username='{}' LIMIT 1 OFFSET 0".format(
@@ -25,25 +26,31 @@ def do_payment(db, session):
     ).fetchone()
     payment_amount = int(request.forms.get('amount'))
     error = None
-    if (sender.get_coins() < payment_amount):
+    if (session.get_id() != request.forms.get("session_id")):
         response.status = 400
-        error = "Not enough funds."
-    elif (payment_amount < 0):
-        response.status = 400
-        error = "Payment amount cannot be negative."
-    elif (recipient is None):
-        response.status = 400
-        error = "Recipient {} does not exist.".format(request.forms.get('recipient'))
-    elif (recipient['username'] == sender.username):
-        response.status = 400
-        error = "Cannot pay self."
+        error = "Invalid session ID."
     else:
-        sender.debit_coins(payment_amount)
-        db.execute(
-            "UPDATE users SET coins={} WHERE users.username='{}'".format(
-                recipient['coins'] + payment_amount, recipient['username']
+        if (sender.get_coins() < payment_amount):
+            response.status = 400
+            error = "Not enough funds."
+        elif (payment_amount < 0):
+            response.status = 400
+            error = "Payment amount cannot be negative."
+        elif (recipient is None):
+            response.status = 400
+            error = "Recipient {} does not exist.".format(request.forms.get('recipient'))
+        elif (recipient['username'] == sender.username):
+            response.status = 400
+            error = "Cannot pay self."
+        else:
+            sender.debit_coins(payment_amount)
+            db.execute(
+                "UPDATE users SET coins={} WHERE users.username='{}'".format(
+                    recipient['coins'] + payment_amount, recipient['username']
+                )
             )
-        )
+
+
     return template(
         "profile",
         user=sender,
